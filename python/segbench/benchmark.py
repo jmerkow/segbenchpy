3297f239be8a3626f8ce48494ae8c066c9815412
jameson <jmerkow@gmail.com>
2015-08-09 12:02:58 -0700

    fixed wrappers

--- python/segbench/benchmark.py
+++ python/segbench/benchmark.py
@@ -11,12 +11,12 @@ def matchEdgeMaps2D(bmap1, bmap2, maxDist=0.005, outlierCost=100, degree=6):
     m1 = zeros_like(bmap1)
     m2 = zeros_like(bmap2)
     cost = benchmark_swig._matchEdgeMaps2D(bmap1, bmap2, m1, m2, 
-        maxDist=maxDist, outlierCost=outlierCost, degree=degree)
+        maxDist, outlierCost, degree)
     return cost,m1,m2
 
-def matchEdgeMaps3D(bmap1, bmap2, maxDist, outlierCost, degree=6):
+def matchEdgeMaps3D(bmap1, bmap2, maxDist=.005, outlierCost=100, degree=6):
     m1 = zeros_like(bmap1)
     m2 = zeros_like(bmap2)
     cost = benchmark_swig._matchEdgeMaps3D(bmap1, bmap2, m1, m2, 
-        maxDist=maxDist, outlierCost=outlierCost,degree=degree)
+        maxDist, outlierCost, degree)
     return cost,m1,m2
